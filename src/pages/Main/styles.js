4577db1be86db9bd9a5e0c0324b1ac4219f1bd92
Julio Gaiotto <juliogaiotto@hotmail.com>
2019-09-02 21:16:15 -0300

    criando css global

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,12 +1,5 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-  font-size: 23px;
-  color: ${props => (props.error ? 'red' : 'blue')};
-  font-family: Arial, Helvetica, sans-serif;
-
-  small {
-    font-size: 16px;
-    color: #333;
-  }
+  color: #fff;
 `;
