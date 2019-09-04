cb9a26193629c595d33974cb3fd906a0a5be7253
Julio Gaiotto <juliogaiotto@hotmail.com>
2019-09-04 13:00:40 -0300

    salvando repositório na state

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-import { Title } from './styles';
+import api from '../../services/api';
 
-export default function Main() {
-  return <Title>Hello</Title>;
+import { Container, Form, SubmitButton } from './styles';
+
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+  };
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    const { newRepo } = this.state;
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...this.state.repositories, data],
+      newRepo: '',
+    });
+  };
+
+  render() {
+    const { newRepo } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            id="input"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton>
+            <FaPlus color="#fff" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
